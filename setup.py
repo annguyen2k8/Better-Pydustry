8d059818c1497ba6134f02bf6c13c853d2f58391
annguyen2k8 <hacker.an1901@gmail.com>
2025-02-12 21:15:17 +0700

    fixed: Import pydustry isn't work (Because I forgot adding setup.py)

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="pydustry",
+    version="0.0.1",
+    author="annguyen2k8",
+    description="A module check info server mindustry.",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/annguyen2k8/Better-Pydustry"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
