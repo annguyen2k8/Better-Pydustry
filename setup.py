8be355b7acfce9b30410c75e2e6f956f01e6f539
annguyen2k8 <hacker.an1901@gmail.com>
2025-02-12 21:18:38 +0700

    ... fck pypip

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="A module check info server mindustry.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/annguyen2k8/Better-Pydustry"
+    url="https://github.com/annguyen2k8/Better-Pydustry",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
